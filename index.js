52af1b198a4fb5b1e6f04dbd92037cbee01e15cb
Farah Arar <farah02arar@gmail.com>
2023-10-05 08:47:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+drivers=[
+    {name:"driver1",hometown:"hometown1"},
+    {name:"driver2",hometown:"hometown2"},
+    {name:"driver3",hometown:"hometown3"},
+       ]
+
+function findMatching(drivers, name) {
+        return drivers.filter(driver=>driver.toLowerCase()===name.toLowerCase());
+}
+      
+function fuzzyMatch(drivers, letters) {
+        return drivers.filter(driver=>driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+      
+function matchName(drivers, name) {
+        return drivers.filter(driver=>driver.name.toLowerCase()===name.toLowerCase());
+}
